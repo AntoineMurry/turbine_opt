07ea5d10999d879ca24f844dd41c5168225f61ed
Antoine <antoine.murry@mazars.fr>
2019-05-30 17:25:50 +0200

    start programming simulated anhealing

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ clean:
 	@rm -fr __pycache__
 	@rm -fr build
 	@rm -fr dist
-	@rm -fr zf_twitter-*.dist-info
-	@rm -fr zf_twitter.egg-info
+	@rm -fr turbine-opt-*.dist-info
+	@rm -fr turbine-opt.egg-info
 
 all: clean install test check_code
 
@@ -40,7 +40,7 @@ check_code:
 
 test:
 	@coverage run -m unittest tests/*.py
-	@coverage report -m --omit=$(VIRTUAL_ENV)/lib/python*,zf_twitter/*
+	@coverage report -m --omit=$(VIRTUAL_ENV)/lib/python*,turbine-opt/*
 ftest:
 	@Write me
 
--- turbine-opt/lib.py
+++ turbine-opt/lib.py
@@ -10,6 +10,8 @@ import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
+import numpy.random as rn
+
 from papillon.data import DATA_SOURCE
 
 pd.set_option('display.width', 200)
@@ -42,11 +44,98 @@ def cpt_blades_imbalance(disk):
     return fx, fy
 
 
+class Simulated_annealing:
+    """
+    encapsulated simulated annealing class
+    """
+    def __init__(self, init_state, chg_state_func, ener_func, temp_func,
+                 max_steps, accept_proba):
+        self.init_state = init_state
+        self.chg_state_func = chg_state_func
+        self.ener_func = ener_func
+        self.temp_func = temp_func
+        self.max_steps = max_steps
+        self.accept_proba = accept_proba
+
+    def optimize(self):
+        state = self.init_state
+        for k in range(self.max_steps):
+            temp = self.temp_func(k, self.max_steps)
+            state_new = self.chg_state_func(state)
+            proba = accept_proba(self.ener_func(state),
+                                 self.ener_func(state_new),
+                                 temp)
+            if proba > np.random.sample():
+                state = state_new
+        return state
+
+
+# temporary functions defined for initial test:
+
+INTERVAL = (-100, 100)
+
+def f(x):
+    """ Function to minimize."""
+    return x ** 2
+
+def clip(x):
+    """ Force x to be in the interval."""
+    a, b = INTERVAL
+    return max(min(x, b), a)
+
+def random_start():
+    """ Random point in the interval."""
+    a, b = INTERVAL
+    return a + (b - a) * rn.random_sample()
+
+
+def cost_function(x):
+    """ Cost of x = f(x)."""
+    return f(x)
+
+
+def random_neighbour(x, fraction=1):
+    """Move a little bit x, from the left or the right."""
+    amplitude = (max(INTERVAL) - min(INTERVAL)) * fraction / 10
+    delta = (-amplitude/2.) + amplitude * rn.random_sample()
+    return clip(x + delta)
+
+
+def acceptance_probability(cost, new_cost, temperature):
+    if new_cost < cost:
+        # print("    - Acceptance probabilty = 1 as new_cost = {} < cost = {}...".format(new_cost, cost))
+        return 1
+    else:
+        p = np.exp(- (new_cost - cost) / temperature)
+        # print("    - Acceptance probabilty = {:.3g}...".format(p))
+        return p
+
+
+def temperature(k, kmax):
+    """ Example of temperature dicreasing as the process goes on."""
+    return max(0.01, min(1, 1 - float(k)/float(kmax)))
+
+
+
 if __name__ == '__main__':
-    # For introspection purposes to quickly get this functions on ipython
-    data_file = os.path.join(DATA_SOURCE, "input-data.csv")
-    disk = read_data(data_file)
-    X = [disk.bl_angle.map(math.cos) * disk.w]
-    Y = [disk.bl_angle.map(math.sin) * disk.w]
-    plt.scatter(X,Y, color='red')
-    plt.show()
+
+    init_state = random_start()
+    chg_state_func = random_neighbour
+    ener_func = cost_function
+    temp_func = temperature
+    max_steps = 100000
+    accept_proba = acceptance_probability
+
+
+    #TODO: finish testing this fist funtion. inspiration = https://perso.crans.org/besson/notebooks/Simulated_annealing_in_Python.html
+
+    Simu = Simulated_annealing(init_state, chg_state_func, ener_func, temp_func,
+            max_steps, accept_proba)
+
+    # plotting unbalance:
+    # data_file = os.path.join(DATA_SOURCE, "input-data.csv")
+    # disk = read_data(data_file)
+    # X = [disk.bl_angle.map(math.cos) * disk.w]
+    # Y = [disk.bl_angle.map(math.sin) * disk.w]
+    # plt.scatter(X,Y, color='red')
+    # plt.show()
