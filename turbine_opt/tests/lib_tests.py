837dc38ea0bbc902ac6a9d0ae37bee8ecdfd5197
Antoine <antoine.murry@mazars.fr>
2019-06-16 15:10:18 +0200

    new blades set for test

--- turbine_opt/tests/lib_tests.py
+++ turbine_opt/tests/lib_tests.py
@@ -1,13 +1,16 @@
 # -*- coding: UTF-8 -*-
 
 # Import from standard library
+import os
+import random
 import unittest
 
 # Import from classical libraries
 import pandas as pd
 
-from turbine_opt.tests.data import DATA_SOURCE_TEST
+from turbine_opt.tests.test_data import DATA_SOURCE_TEST
 from turbine_opt.lib import group_creation
+from turbine_opt.lib import dispose_blades
 
 
 class TestUtils(unittest.TestCase):
@@ -15,9 +18,9 @@ class TestUtils(unittest.TestCase):
     # @unittest.skip('')
     def test_dispose_blades(self):
         random.seed(0)
-        disk = pd.read_csv(os.join(DATA_SOURCE_TEST, "input-data.csv"))
-        group1, group2 = (disk, alpha=0.05)
-        list(disk.w > disk.w.shift(1))
+        disk = pd.read_csv(os.path.join(DATA_SOURCE_TEST, "input-data.csv"))
+        group1, group2 = dispose_blades(disk, alpha=0.05)
+        list(group1.w > group1.w.shift(1))
         self.assertEqual(df.shape, (29134, 3))
 
 
